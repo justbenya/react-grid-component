53027d70e05f67fcf72bd2912d5c7c1469dbcb98
JustBenya <justbenya@gmail.com>
2021-01-19 18:35:04 +0700

    Исправил перевод строки с CRLF → LF

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,34 @@ import Grid from './components/Grid/Grid';
 import GridRuler from './components/GridRuler/GridRuler';
 
 function App() {
-    return (
-        <div className="App" style={ { margin: 16, position: 'relative', height: '100vh' } }>
-            <GridRuler spacing={ 1 } />
-            <Grid
-                container spacing={ 1 }
-                justifyContent={ 'space-between' }
-                alignContent={ 'center' }
-                style={ { height: '100%' } }
-            >
-                <Grid item xs={ 1 } sm={ 6 } md={ 4 } lg={ 3 }>
-                    <Box>Box 1</Box>
-                </Grid>
+  return (
+    <div className="App" style={{ margin: 16, position: 'relative', height: '100vh' }}>
+      <GridRuler spacing={1} />
+      <Grid
+        container
+        spacing={1}
+        justifyContent="space-between"
+        alignContent="center"
+        style={{ height: '100%' }}
+      >
+        <Grid item xs={1} sm={6} md={4} lg={3}>
+          <Box>Box 1</Box>
+        </Grid>
 
-                <Grid item xs={ 1 } sm={ 6 } md={ 4 } lg={ 3 }>
-                    <Box>Box 2</Box>
-                </Grid>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
+          <Box>Box 2</Box>
+        </Grid>
 
-                <Grid item xs={ 1 } sm={ 6 } md={ 4 } lg={ 3 }>
-                    <Box>Box 3</Box>
-                </Grid>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
+          <Box>Box 3</Box>
+        </Grid>
 
-                <Grid item xs={ 1 } sm={ 6 } md={ 4 } lg={ 3 }>
-                    <Box>Box 4</Box>
-                </Grid>
-            </Grid>
-        </div>
-    );
+        <Grid item xs={1} sm={6} md={4} lg={3}>
+          <Box>Box 4</Box>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
